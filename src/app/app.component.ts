2ca1b425963a7fca5be14db944e03a7120a3f056
edcher <86379980+edcher@users.noreply.github.com>
2022-09-09 12:14:52 +0200

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,17 @@
 import { Component, VERSION } from '@angular/core';
 
+class theater {
+  key: string;
+  palco: Array<any>;
+  platea: Array<any>;
+  constructor (key: string, palco: Array<any>, platea: Array<any>) {
+    this.key = key;
+    this.palco = palco;
+    this.platea = platea;
+  }
+
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +19,7 @@ import { Component, VERSION } from '@angular/core';
 })
 export class AppComponent  {
   title: string = 'Prenotazioni teatro';
+  key: string = "904ac10a";
+  platea: Array<any> = new Array(7).fill('').map(() => new Array(10).fill('x'));
+  palchetto: Array<any> = new Array(4).fill('').map(() => new Array(6).fill('x'));
 }
