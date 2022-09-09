f609a8cd7cdffa10780188150a9b04eaa2aea9b6
edcher <86379980+edcher@users.noreply.github.com>
2022-09-09 12:42:55 +0200

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,8 +19,25 @@ class theater {
 })
 export class AppComponent  {
   title: string = 'Prenotazioni teatro';
-  key: string = "904ac10a";
+  key: string = "";
   platea: Array<any> = new Array(7).fill('').map(() => new Array(10).fill('x'));
   palchetto: Array<any> = new Array(4).fill('').map(() => new Array(6).fill('x'));
+
   constructor(private service: TheaterService) { }
+
+  cerca_spettacolo(chiave: string) {
+    if (chiave !== '') {
+      this.service.get_spettacolo(chiave).subscribe({
+        next: (x: any) => {
+          const prenotazione = JSON.parse(x);
+          this.platea = prenotazione.slice(0, 7);
+          this.palchetto = prenotazione.slice(7);
+          this.key = chiave;
+        },
+        error: (err: any) => console.error(`Errore nell'observer: ${JSON.stringify(err)}`)
+      });
+    }
+    else alert("Inserisci una chiave valida.") // se non viene inserito niente nel campo input
+  }
+
 }
--- src/app/theater.service.ts
+++ src/app/theater.service.ts
@@ -1,8 +1,24 @@
+import { Observable } from "rxjs";
 import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
 
-@Injectable()
+@Injectable({
+  providedIn: 'root'
+})
+
+
+
 export class TheaterService {
+  URL: string = "https://eu-central-1.aws.data.mongodb-api.com/app/kvaas-giwjg/endpoint/";
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  get_spettacolo(chiave: string): Observable<string> {
+    return this.http.get<string>(`${this.URL}get?key=${chiave}`);
+  }
+
+  set_spettacolo(chiave: string, teatro: any[]): Observable<string> {
+    return this.http.post<string>(`${this.URL}set?key=${chiave}`, teatro)
+  }
 
 }
